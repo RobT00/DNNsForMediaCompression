0eb3dd0eb776851a74c12c0117ee979c29a63b29
Rob Trew <trewr@tcd.ie>
2020-03-13 22:39:32 +0000

    Added psnr script for gathering metrics

--- Python/Results/psnr_batch.sh
+++ Python/Results/psnr_batch.sh
@@ -2,6 +2,6 @@
 
 batch_input=$1
 output="${2}.json"
-run_batch="python3 $Git/vmaf/run_vmaf_in_batch"
+run_batch="python3 $Git/vmaf/run_psnr_in_batch"
 
 $run_batch $batch_input --out-fmt json --parallelize >> $output
