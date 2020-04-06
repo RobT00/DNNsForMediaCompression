ccf5ebdb85702f7c0193cf6fbdc7f9fc444b2084
Rob Trew <trewr@tcd.ie>
2020-04-06 13:55:28 +0100

    Capturing stream info from video to get more accurate bitrate

--- Python/Results/ffprobe.sh
+++ Python/Results/ffprobe.sh
@@ -3,4 +3,4 @@
 vid=$1
 out_file=${2}.json
 
-ffprobe -v quiet -i "$vid" -print_format json -show_format > "${out_file}"
+ffprobe -v quiet -i "$vid" -print_format json -show_format -show_streams > "${out_file}"
